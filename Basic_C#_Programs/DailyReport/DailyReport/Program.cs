a30547c1a448c8292f85b19b0dbe2557e014e59d
jordanritzie19 <jordanritzie@yahoo.com>
2023-02-20 02:35:27 -0500

    A few more basic programs, including a Daily Report

---
+++ Basic_C#_Programs/DailyReport/DailyReport/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main()
+        {
+            //header
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report.");
+
+            //report questions, asks a question, and prompts for a response
+            Console.WriteLine("What is your name?");  //this is an example of a question
+            string yourName = Console.ReadLine();   //your response gets stored in this variable
+            Console.WriteLine("What course are you on?");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            int pageNumber = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything? Please answer with \"true\" or \"false\".");
+            bool needsHelp = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string positiveXP = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            int hoursStudied = Console.ReadLine();
+
+            //closer
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
