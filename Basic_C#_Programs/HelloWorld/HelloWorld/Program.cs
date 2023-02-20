f0dcfeb90726d20f15bd887e92ff5e7d15d721ef
jordanritzie19 <jordanritzie@yahoo.com>
2023-02-20 01:48:44 -0500

    First Programs

---
+++ Basic_C#_Programs/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World");
+        Console.WriteLine("The use of double quotes \" \" is necessary");
+        Console.ReadLine();
+    }
+}
+
