b298f57fb1bedacd90dbfba5b549aaafdde1a446
jordanritzie19 <jordanritzie@yahoo.com>
2023-02-23 00:00:47 -0500

    Loops

---
+++ Basic_C#_Programs/whileDoWhile_program/whileDoWhile_program/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileDoWhile_program
+{
+    class Program
+    {
+        static void Main()
+        {
+            int counter = 1; //initializes count to 1
+            while (counter < 4)  //loops until counter reaches 4
+            {
+                Console.WriteLine(counter); //prints current count
+                counter++;  //and then increments
+            }
+            
+            do  //runs a counting loop once and then...  (check while)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+            while (counter > 4 && counter < 7);  //keeps looping if between but not 4 and 7 
+            Console.ReadLine();  //keeps console open
+        }
+    }
+}
