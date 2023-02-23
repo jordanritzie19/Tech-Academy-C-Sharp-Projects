12d8c7bd9788b2815ce716c5e51063d99a0ac88c
jordanritzie19 <jordanritzie@yahoo.com>
2023-02-22 22:36:26 -0500

    Boolean programs

---
+++ Basic_C#_Programs/BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            //Console.WriteLine(true ^ true);   XOR operator
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            Console.WriteLine(true && true && true || false);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/carInsuranceQualification_program/carInsuranceQualification_program/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace carInsuranceQualification_program
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine()); //gets a value for age
+
+            Console.WriteLine("Have you ever had a DUI? Answer \"True\" or \"False\"");
+            bool hadDUI = Convert.ToBoolean(Console.ReadLine());  //gets a boolean value for whether or not you've had a DUI
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());  //gets number of speeding tickets
+
+            bool isQualified = age > 15 && hadDUI == false && tickets <= 3;  //checks if eligible for car insurance
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(isQualified); //if qualified prints true, else false
+            Console.ReadLine();  //keeps console open
+        }
+    }
+}
