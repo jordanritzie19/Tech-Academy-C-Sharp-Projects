abeedc289faacdf9269c40f445d3225101f7cf92
jordanritzie19 <jordanritzie@yahoo.com>
2023-03-06 01:23:18 -0500

    Classes and calling methods

---
+++ Basic_C#_Programs/callingMethods/callingMethods/ModdedNumber.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace callingMethods
+{
+    public class ModdedNumber
+    {
+        public ModdedNumber(int initial)  //constructor with parameter of an initial number to modify
+        {
+            Value = TimesThree(initial);  //multiplies initial number by 3
+            Value = PlusThree(Value);    //adds 3
+            Value = TimesThree(Value);  //...
+            Value = GetIntArray(Value)[0] + GetIntArray(Value)[1]; //adds the two digits together.
+
+
+            // methods
+            int PlusThree(int num)  //takes in an argument for parameter num, and adds 3 to it
+            {
+                num = num + 3;
+                return num;
+            }
+            int TimesThree(int num) //takes in argument for parameter num, multiplying it by 3
+            {
+                num = num * 3;
+                return num;
+            }
+            int[] GetIntArray(int num) //adds the 2 digits together
+            {
+                List<int> listOfInts = new List<int>(); //this is what the separated number goes into
+                while (num > 0) //'chop' a number until theres nothing remaining
+                {
+                    //divide 10 and take remainder, in decimal system this is the same as taking the last digit of a number
+                    listOfInts.Add(num % 10); //ex: 54 -> 50 % 10 = 4
+                    //chop the last digit, integers always effectively round down
+                    num = num / 10; // ex: 54/10 -> 5.4 (stored as integer '5')
+                }
+                listOfInts.Reverse(); //without this, 54 would be split into [4, 5] as 
+                //our method starts from the end of the number, this reflects it back as [5, 4] as we want
+                return listOfInts.ToArray();
+            }
+        }
+        public int Value { get; } //this variable cannot be directly changed by 'main'
+    }
+}
---
+++ Basic_C#_Programs/callingMethods/callingMethods/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace callingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number from 1-9");   //input single digit number
+            int startingNumber = Convert.ToInt32(Console.ReadLine());  //inputted number
+            try  //this makes sure that the chosen number is actually between 1 and 9
+            {
+                if (startingNumber > 9 || startingNumber < 1)
+                {
+                    throw new ArithmeticException("You chose a number outside the scope of this program.");
+                }
+            }
+            catch (ArithmeticException er)
+            {
+                Console.WriteLine(er); //writes out the above arithmetic exception (line 19)
+                Console.ReadLine();
+                return;  //this ends the program
+            }
+            //I did not desire a finally{} in this case
+            ModdedNumber guess = new ModdedNumber(startingNumber);  //sets the initial value of ModdedNumber 'guess' to inputted number from start of program
+            Console.WriteLine("Take that number and multiply it by 3"); //this is just going through the steps for this math trick
+            Console.ReadLine();
+            Console.WriteLine("Now add 3 to that number"); 
+            Console.ReadLine();
+            Console.WriteLine("Multiply your new number by 3 once again");
+            Console.ReadLine();
+            Console.WriteLine("Add the digits together.");
+            Console.ReadLine();
+            Console.WriteLine("Is your new number... a " + guess.Value + "?"); //this should ALWAYS be a 9
+            Console.ReadLine(); //keeps program open
+
+        }
+    }
+}
