065608f9dc87cd9fd3375dc86648bdc3192778ed
jordanritzie19 <jordanritzie@yahoo.com>
2023-03-13 01:04:29 -0400

    Output parameters, overloaded methods, static modifier

---
+++ Basic_C#_Programs/ClassSubmission Assignment/ClassSubmission Assignment/Divider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission_Assignment
+{
+    class Divider
+    {
+        public void Divide(int x)  //this function doesn't return a value since it has void keyword
+        {
+            x = x / 2; //divides input by 2
+            Console.WriteLine(x); //prints new value to console
+        }
+
+        public static int Divide(int x, out int y)  //static is unchanging, theres only one copy
+        {
+            y = x / 2;  //sets the new y value to be sent back out to main()
+            return x / y;  //returns the numbers divided (always a 2)
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/ClassSubmission Assignment/ClassSubmission Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Divider dividend = new Divider();  //instantiates the divider 'dividend'
+            Console.WriteLine("Give me a number: ");
+            dividend.Divide(Convert.ToInt32(Console.ReadLine()));  //sends in inputted number as argument in class method. This pastes new number
+
+            int x = 26, y = 52;  //some values to use later
+            Console.WriteLine("Currently y = 52");  //this is used as proof of output parameter
+            int j = Divider.Divide(x, out y);  //y's value gets changed in method to be half of value of x
+            Console.WriteLine("But now its half " + x + " aka " + y);  //proof of y changing
+            Console.WriteLine(j);  //the int value returned from Divider.Divide method above. Should always be '2'
+            Console.ReadLine();  //pauses program
+        }
+    }
+}
