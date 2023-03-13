3594d23613abc0cf8536c279853ac3c203eda953
jordanritzie19 <jordanritzie@yahoo.com>
2023-03-12 22:49:03 -0400

    void methods and class instantiation

---
+++ Basic_C#_Programs/MethodClassAssignment/MethodClassAssignment/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Class1
+    {
+        public void holdTwo(int x, int y)  //void means a value isn't returned
+        {
+            x += 4;  //adds 4 to x, though this is never used :(
+            Console.WriteLine(y);  //prints out argument y
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 instance = new Class1(); //instantiatiation of 'Class1' class
+            instance.holdTwo(2, 4); //x =2, y=4. This should print '4'
+            instance.holdTwo(y: 4, x: 6); //named parameters can be out of order, this should print '4'
+            Console.ReadLine(); //pauses program
+        }
+
+    }
+}
