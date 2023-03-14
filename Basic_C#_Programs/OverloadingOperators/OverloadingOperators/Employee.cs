196080b421d44591ae9a9447540ac5bf1338dc05
jordanritzie19 <jordanritzie@yahoo.com>
2023-03-14 11:03:02 -0400

    Overloaded Operators

---
+++ Basic_C#_Programs/OverloadingOperators/OverloadingOperators/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperators
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator ==(Employee employee1, Employee employee2)  //takes in 2 employee objects
+        {
+            return employee1.Id == employee2.Id;  //and returns boolean of id1 == id2
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)  //overloaded == means != needs defined as well
+        {
+            return employee1.Id != employee2.Id;
+        }
+
+        // resolved warning using this link: https://learn.microsoft.com/en-us/dotnet/csharp/misc/cs0660?f1url=%3FappId%3Droslyn%26k%3Dk(CS0660)  
+        public override bool Equals(object o)
+        {
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+        //end of code resolving warning
+    }
+}
---
+++ Basic_C#_Programs/OverloadingOperators/OverloadingOperators/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Jack = new Employee() { FirstName = "Jack", LastName = "Miller", Id = 1000 };    //instiatiating some Employees
+            Employee Jack2 = new Employee() { FirstName = "Jack", LastName = "Larange", Id = 1000 };  //this represents Jack after he changed his last name
+            Employee Mike = new Employee() { FirstName = "Mike", LastName = "McDaniels", Id = 1001 };
+
+            bool isSame = Jack == Mike; //id of jack == id of mike? using overloaded ==
+            bool isDif = Jack != Mike;  //uses counterpart overloaded operator !=
+            bool isSame2 = Jack == Jack2;  
+            
+            Console.WriteLine("Ids of Jack and Mike are the same: " + isSame);  //should read False, True, True
+            Console.WriteLine("Ids of Jack and Mike are different: " + isDif);
+            Console.WriteLine("Ids of Mike before and after changing last name are the same: " + isSame2);
+
+            Console.ReadLine();
+        }
+    }
+}
