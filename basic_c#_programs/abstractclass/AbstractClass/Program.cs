6a2769f74214e2230da4cb99990c21d2d9cc77ec
jordanritzie19 <jordanritzie@yahoo.com>
2023-03-15 07:36:51 -0400

    interface polymorphism

---
+++ basic_c#_programs/abstractclass/AbstractClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Sample = new Employee() { firstName = "Sample", lastName = "Student" };  //instantiation of Employee
+            Console.WriteLine("Employee " + Sample.firstName + " " + Sample.lastName + " is working.");
+            //Sample.SayName();  //prints full name of employee Sample
+            Console.WriteLine(Sample.IsWorking);
+
+            Console.WriteLine(Sample.firstName + " has found better opportunities and has left the company");
+            Console.WriteLine(Sample.firstName + " " + Sample.lastName + " works here.");
+            Sample.Quit();
+            Console.WriteLine(Sample.IsWorking);
+
+            IQuittable quitter = new Employee();  //polymorphism
+            quitter.Quit();
+            Console.ReadLine(); //pauses the program
+
+            
+        }
+    }
+
+}
