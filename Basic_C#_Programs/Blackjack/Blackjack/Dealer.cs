36c738644dc94badf93ce9174c2245dba70078e2
jordanritzie19 <jordanritzie@yahoo.com>
2023-03-23 00:40:10 -0400

    dateTime

--- Basic_C#_Programs/Blackjack/Blackjack/Dealer.cs
+++ Basic_C#_Programs/Blackjack/Blackjack/Dealer.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Blackjack
 {
-    class Dealer
+    public class Dealer
     {
         public string Name { get; set; }
         public Deck Deck { get; set; }
@@ -15,7 +16,13 @@ namespace Blackjack
         public void Deal(List<Card> Hand)
         {
             Hand.Add(Deck.Cards.First());
-            Console.WriteLine(Deck.Cards.First().ToString() + "\n");
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\jorda\Logs\log.txt", true))
+            {
+                file.WriteLine(DateTime.Now);
+                file.WriteLine(card);
+            }
             Deck.Cards.RemoveAt(0);
         }
     }
---
+++ Basic_C#_Programs/dateTime/dateTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;  //instatiates current time
+            Console.WriteLine(currentTime);  //prints it to console
+
+            Console.WriteLine("Give me a number: ");       //instantiates hours added
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("In {0} hours, it will be {1}",hours,currentTime.AddHours(hours)); //uses AddHours method, passing along the hours input from above
+            Console.ReadLine();
+        }
+    }
+}
